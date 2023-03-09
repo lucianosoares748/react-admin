700b0a19095e49cde4c83df84a48cc0668f409b7
luciano soares <lucianosoaresdev748@gmail.com>
2023-03-09 17:52:34 -0300

    error color mode solved

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 // eslint-disable-next-line no-unused-vars
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Topbar } from "./scenes/global/Topbar";
+import Topbar from "./scenes/global/Topbar";
 
-import "./App.css";
+import "./App";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    // eslint-disable-next-line no-undef
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
@@ -17,7 +17,6 @@ function App() {
             <Topbar />
           </main>
         </div>
-        ;
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
